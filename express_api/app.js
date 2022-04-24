9a0e5b28a7ca01c95b5bca0aaab8106affb2dab5
Ingrzs <ing.ramirozs@gmail.com>
2022-04-24 15:26:49 -0500

    primer endpoint completo

--- express_api/app.js
+++ express_api/app.js
@@ -1,18 +1,18 @@
-const express = require ('express')
+const express = require('express')
 
 const app = express()
-app.use(express.json) //para decirle que usaremos json
+app.use(express.json()) //para decirle que usaremos json
 
 const port = 3000
 
 
-app.get('/v1/explorers', (req,res)=>{
+app.get('/v1/explorers', (req,res) => {
     console.log(`api explores ${new Date()}`)
-    const explore1 = {id:1, name:"rami1"}
-    const explore2 = {id:2, name:"rami2"}
-    const explore3 = {id:3, name:"rami3"}
-    const explore4 = {id:4, name:"rami4"}
-    const explorers = [explore1,explore2,explore3,explore4]
+    const explorer1 = {id:1, name:"rami1"}
+    const explorer2 = {id:2, name:"rami2"}
+    const explorer3 = {id:3, name:"rami3"}
+    const explorer4 = {id:4, name:"rami4"}
+    const explorers = [explorer1,explorer2,explorer3,explorer4]
     res.status(200).json(explorers)
 
 })
